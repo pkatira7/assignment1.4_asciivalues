03eec030ce111843d0f3d95d001245d4e0aa0301
pkatira7 <pkatira@vmware.com>
2016-12-23 15:07:50 +0530

    Add files via upload

---
+++ AsciiValues.java
@@ -0,0 +1,13 @@
+
+public class AsciiValues {
+
+public static void main (String [] args)
+{
+	char c ; 
+   for (int a = 65 ; a <=90 ; a ++) 
+   {
+	  c = (char) a ;
+	  System.out.println (c+"-"+a) ;  
+   } 
+}
+}
